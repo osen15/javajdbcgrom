28337ae1d0b14aa093666ba3b4c479a072ad1adb
Oleg <senyuholeg@gmail.com>
2018-01-21 15:34:42 +0100

    fix

--- src/main/java/JDBCFirstStep.java
+++ src/main/java/JDBCFirstStep.java
@@ -1,5 +1,3 @@
-import com.sun.xml.internal.bind.v2.TODO;
-
 import java.sql.*;
 
 public class JDBCFirstStep {
@@ -9,33 +7,25 @@ public class JDBCFirstStep {
     private static final String USER = "main";
     private static final String PASS = "11111111";
 
-    public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
-
-            //1.DB Driver
-            //2.Create connection
-            //3.Create query/statement
-            //4.Execute query
-            //5.Work with result
-            //6.Close all the connection
-
+    public ResultSet jdbcStart() {
+        ResultSet example = null;
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+             Statement statement = connection.createStatement()) {
             try {
                 Class.forName(JDBC_DRIVER);
             } catch (ClassNotFoundException e) {
                 System.out.println("Class" + JDBC_DRIVER + " not found");
-                return;
-            }
-            try (ResultSet resultSet = statement.executeQuery("SELECT * FROM Test")) {
-                while (resultSet.next()) {
-                    //TODO;
-                }
 
             }
+            try (ResultSet resultSet = statement.executeQuery("SELECT * FROM Test")) {
+                example = resultSet;
+            }
+
         } catch (SQLException e) {
             System.err.println("Something went wrong");
             e.printStackTrace();
         }
+
+        return example;
     }
-
-
 }

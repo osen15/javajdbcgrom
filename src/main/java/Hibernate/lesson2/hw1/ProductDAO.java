497a17bdf91353fb22d968a80be0cdad9b022fe1
Oleg <senyuholeg@gmail.com>
2018-04-24 20:18:28 +0200

    fix

--- src/main/java/Hibernate/lesson2/hw1/ProductDAO.java
+++ src/main/java/Hibernate/lesson2/hw1/ProductDAO.java
@@ -8,7 +8,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
-import java.util.ArrayList;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,60 +16,67 @@ import java.util.Map;
 
 public class ProductDAO {
     private SessionFactory sessionFactory;
-    private HashMap<Integer, Object> putResults = new HashMap<>();
+    private String FIND_BY_ID = "FROM Product WHERE ID = :ID";
+    private String FIND_BY_NAME = "FROM Product WHERE NAME = :NAME?";
+    private String CONTAINED_NAME = "FROM Product WHERE NAME LIKE?";
+    private String FIND_BY_PRICE = "FROM Product WHERE PRICE  >= DIFDELTA AND PRICE <= SUMDELTA";
+    private String FIND_BY_NAME_SORTED_ASC = "FROM Product WHERE NAME = :NAME ORDER BY NAME ASC";
+    private String FIND_BY_NAME_SORTED_DESC = "FROM Product WHERE NAME = :NAME ORDER BY NAME DESCX";
+    private String FIND_BY_PRICE_SORTED_DESC = "FROM Product  WHERE PRICE >= DIFDELTA AND PRICE <= SUMDELTA ORDER BY PRICE DESC";
 
     public Product findById(Long id) {
-        String findById = "from Product where id = ?";
-        putResults.put(0, id);
-        return genParameter(findById, putResults).get(0);
+        HashMap<String, Object> putResults = new HashMap<>();
+        putResults.put("ID", id);
+        return genParameter(FIND_BY_ID, putResults).get(0);
     }
 
     public List findByName(String name) {
+        HashMap<String, Object> putResults = new HashMap<>();
         String findByName = "from Product where name = ?";
-        putResults.put(0, name);
-        return genParameter(findByName, putResults);
+        putResults.put("NAME", name);
+        return genParameter(FIND_BY_NAME, putResults);
     }
 
     public List containedName(String name) {
+        HashMap<String, Object> putResults = new HashMap<>();
         String containedName = "from Product where name like ?";
-        putResults.put(0, "%" + name + "%");
-        return genParameter(containedName, putResults);
+        putResults.put("NAME", "%" + name + "%");
+        return genParameter(CONTAINED_NAME, putResults);
 
     }
 
     public List findByPrice(Integer price, Integer delta) {
-        String findByPrice = "from Product  where price >= ? and price <= ?";
-        putResults.put(0, price - delta);
-        putResults.put(1, price + delta);
-        return genParameter(findByPrice, putResults);
+        HashMap<String, Object> putResults = new HashMap<>();
+        putResults.put("DIFDELTA", price - delta);
+        putResults.put("SUMDELTA", price + delta);
+        return genParameter(FIND_BY_PRICE, putResults);
     }
 
     public List findByNameSortedAsc(String name) {
-        String findByNameSortedAsc = "from Product where name = ? order by name asc";
-        putResults.put(0, name);
-        return genParameter(findByNameSortedAsc, putResults);
+        HashMap<String, Object> putResults = new HashMap<>();
+        putResults.put("NAME", name);
+        return genParameter(FIND_BY_NAME_SORTED_ASC, putResults);
     }
 
     public List findByNameSortedDesc(String name) {
-        String findByNameSortedDesc = "from Product where name = ? order by name desc";
-        putResults.put(0, name);
-        return genParameter(findByNameSortedDesc, putResults);
+        HashMap<String, Object> putResults = new HashMap<>();
+        putResults.put("NAME", name);
+        return genParameter(FIND_BY_NAME_SORTED_DESC, putResults);
     }
 
 
     public List findByPriceSortedDesc(Integer price, Integer delta) {
-
-        String findByPrice = "from Product  where price >= ? and price <= ? order by price desc";
-        putResults.put(0, price - delta);
-        putResults.put(1, price + delta);
-        return genParameter(findByPrice, putResults);
+        HashMap<String, Object> putResults = new HashMap<>();
+        putResults.put("DIFDELTA", price - delta);
+        putResults.put("SUMDELTA", price + delta);
+        return genParameter(FIND_BY_PRICE_SORTED_DESC, putResults);
     }
 
-    private List<Product> genParameter(String hql, HashMap<Integer, Object> objectMap) {
+    private List<Product> genParameter(String hql, HashMap<String, Object> objectMap) {
         List<Product> products = null;
         try (Session session = createSessionFactory().openSession()) {
             Query query = session.createQuery(hql);
-            for (Map.Entry<Integer, Object> entry : objectMap.entrySet())
+            for (Map.Entry<String, Object> entry : objectMap.entrySet())
                 query.setParameter(entry.getKey(), entry.getValue());
             products = query.getResultList();
 
--- src/main/java/Hibernate/lesson2/hw2/Demo.java
+++ src/main/java/Hibernate/lesson2/hw2/Demo.java
@@ -5,8 +5,8 @@ public class Demo {
     public static void main(String[] args) {
        ProductDAO productDAO = new ProductDAO();
         // System.out.println(productDAO.findById((long) 48458585));
-         System.out.println(productDAO.findByName("sss"));
-        // System.out.println(productDAO.containedName("robocop"));
+         //System.out.println(productDAO.findByName("sss"));
+         System.out.println(productDAO.containedName("robocop"));
         // System.out.println(productDAO.findByNameSortedAsc("robocop"));
         // System.out.println(productDAO.findByPrice(100,20));
         // System.out.println(productDAO.findByNameSortedDesc("test"));
